603687f418520909d3f51ad2b9e1ca4d4f5ea551
Tobias Happ <tobias.happ@gmx.de>
2020-09-02 20:02:02 +0200

    Add image directories

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,22 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
+        path: `${__dirname}/static/img`,
+        name: 'uploads',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
         path: `${__dirname}/src/pages`,
         name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/img`,
+        name: 'images',
       },
     },
     {
