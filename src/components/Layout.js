9dda39dbd9e9c083bda6094b8ec49c2cd49b9d43
Tobias Happ <tobias.happ@gmx.de>
2020-11-22 15:37:54 +0100

    Add example styled component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import styled from 'styled-components'
 
-const TemplateWrapper = ({ children }) => {
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Layout = ({ children }) => {
   return (
     <>
       <Helmet>
         <html lang="de" />
       </Helmet>
+      <Title>Nice title</Title>
       {children}
     </>
   )
 }
 
-export default TemplateWrapper
+export default Layout
