5c09e1bb6b14d473587734d494dbc9c94d13f3e2
Tobias Happ <tobias.happ@gmx.de>
2021-01-16 17:40:25 +0100

    Enable styled-components in previews
    See https://github.com/netlify/netlify-cms/issues/1408#issuecomment-526851039

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,6 @@
 import CMS from 'netlify-cms-app'
 
 import IndexPreview from './preview-templates/IndexPreview'
+import withStyledComponentsRendered from '../util/withStyledComponentsRendered'
 
-CMS.registerPreviewTemplate('index', IndexPreview)
+CMS.registerPreviewTemplate('index', withStyledComponentsRendered(IndexPreview))
