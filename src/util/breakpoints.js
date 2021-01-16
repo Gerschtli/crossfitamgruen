d956bb11c25d7fc5be20e08e475a0de288cf79d3
Tobias Happ <tobias.happ@gmx.de>
2021-01-16 17:07:44 +0100

    Add breakpoint and color configs

---
+++ src/util/breakpoints.js
@@ -0,0 +1,4 @@
+export const BREAKPOINTS = {
+  sm: '480px',
+  md: '800px',
+}
---
+++ src/util/colors.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+  primary: {
+    default: 'rgb(255,171,31)',
+    dark: 'rgb(222,148,25)',
+  },
+  secondary: {
+    default: 'rgb(165,208,168)',
+  },
+  dark: 'rgb(39,39,39)',
+  light: 'rgb(255,255,255)',
+  shadow: 'rgba(0,0,0,0.87)',
+}
