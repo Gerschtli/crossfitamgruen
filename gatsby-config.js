ad9e42d3f0dfa9e09516e15cf3f8dfa5bcc0a712
Tobias Happ <tobias.happ@gmx.de>
2021-01-19 22:07:35 +0100

    Enable image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,9 +23,19 @@ module.exports = {
         name: 'images',
       },
     },
+    // Including in your Gatsby plugins will transform any paths in your frontmatter
+    `gatsby-plugin-netlify-cms-paths`,
+    // Including in your Remark plugins will transform any paths in your markdown body
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-plugin-netlify-cms-paths`,
+        ],
+      },
     },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
