509fb46c06f06b5c28fcf4e4e4ea7be5e457c440
Tobias Happ <tobias.happ@gmx.de>
2021-01-27 00:16:34 +0100

    Add link to disclosure in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'gatsby'
 
 import { colors } from '../util/colors'
 import JournalLink from './JournalLink'
@@ -7,7 +8,8 @@ import { breakpoints, mediaQuery } from '../util/breakpoints'
 
 const StyledFooter = styled.footer`
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
+  align-items: center;
   padding: 1em;
   background-color: ${colors.dark};
 
@@ -16,9 +18,15 @@ const StyledFooter = styled.footer`
     padding-right: 1.5em;
   }
 `
+const StyledLink = styled(Link)`
+  color: ${colors.light};
+  text-decoration: none;
+  font-size: .8em;
+`
 
 const Footer = () => (
   <StyledFooter>
+    <StyledLink to={'/datenschutz'}>Datenschutz</StyledLink>
     <JournalLink />
   </StyledFooter>
 )
