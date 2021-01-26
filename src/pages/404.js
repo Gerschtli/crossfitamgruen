2d0857fd5adbde77fdac9839af841f0a0998edee
Tobias Happ <tobias.happ@gmx.de>
2021-01-26 23:55:46 +0100

    Style not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Layout from '../components/Layout'
+import { breakpoints } from "../util/breakpoints";
 
-const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const StyledContent = styled.main`
+  margin: 0 auto;
+  max-width: ${breakpoints.md}em;
+  padding: 1em;
+`
+
+const NotFoundPage = () => {
+  const title = "Seite nicht gefunden";
+
+  return (
+    <Layout title={title} description={title}>
+      <StyledContent>
+        <h1>{title}</h1>
+        <p>Entschuldigung, aber es scheint, als gäbe es diese Seite nicht...</p>
+      </StyledContent>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
