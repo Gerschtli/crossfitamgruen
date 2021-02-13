16acd7e9ed64d90d5977764338b415eb10dfa685
Tobias Happ <tobias.happ@gmx.de>
2021-02-13 16:48:28 +0100

    Extract SimplePage

---
+++ src/components/templates/SimplePage.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Layout from '../base/Layout'
+import Content from '../util/Content'
+import { breakpoints } from '../../util/breakpoints'
+import { graphql } from 'gatsby'
+import { normalizePageInputWithBody } from '../../util/normalizer'
+
+const StyledContent = styled(Content)`
+  margin: 0 auto;
+  max-width: ${breakpoints.md}em;
+  padding: 1em;
+
+  p {
+    margin-top: 1em;
+    margin-bottom: 1em;
+    line-height: 1.4;
+  }
+
+  ul,
+  ol {
+    padding-left: 2em;
+  }
+
+  ul p,
+  ol p,
+  ul ul,
+  ul ol,
+  ol ul,
+  ol ol {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  // necessary for preview
+  img {
+    display: block;
+    margin: 0 auto;
+  }
+`
+const SimplePage = ({ data, entry, widgetFor }) => {
+  const { dataSet, html } = normalizePageInputWithBody(data, entry, widgetFor)
+
+  return (
+    <Layout title={dataSet.title} description={dataSet.description}>
+      <StyledContent content={html} isPreview={!data} />
+    </Layout>
+  )
+}
+
+export default SimplePage
+
+export const query = graphql`
+  fragment SimplePageFragment on MarkdownRemark {
+    frontmatter {
+      title
+      description
+    }
+    html
+  }
+`
--- src/templates/disclosure.js
+++ src/templates/disclosure.js
@@ -1,69 +1,13 @@
 import { graphql } from 'gatsby'
-import React from 'react'
-import styled from 'styled-components'
 
-import Layout from '../components/base/Layout'
-import Content from '../components/util/Content'
-import { breakpoints } from '../util/breakpoints'
+import SimplePage from '../components/templates/SimplePage'
 
-const StyledContent = styled(Content)`
-  margin: 0 auto;
-  max-width: ${breakpoints.md}em;
-  padding: 1em;
-
-  p {
-    margin-top: 1em;
-    margin-bottom: 1em;
-    line-height: 1.4;
-  }
-
-  ul,
-  ol {
-    padding-left: 2em;
-  }
-
-  ul p,
-  ol p,
-  ul ul,
-  ul ol,
-  ol ul,
-  ol ol {
-    margin-top: 0;
-    margin-bottom: 0;
-  }
-
-  // necessary for preview
-  img {
-    display: block;
-    margin: 0 auto;
-  }
-`
-
-const DisclosurePage = ({ data, entry, widgetFor }) => {
-  // data is not set in preview mode
-  const dataSet = data
-    ? data.markdownRemark.frontmatter
-    : entry.getIn(['data']).toJS()
-
-  const html = data ? data.markdownRemark.html : widgetFor('body')
-
-  return (
-    <Layout title={dataSet.title} description={dataSet.description}>
-      <StyledContent content={html} isPreview={!data} />
-    </Layout>
-  )
-}
-
-export default DisclosurePage
+export default SimplePage
 
 export const pageQuery = graphql`
   query DisclosureTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "disclosure" } }) {
-      frontmatter {
-        title
-        description
-      }
-      html
+      ...SimplePageFragment
     }
   }
 `
