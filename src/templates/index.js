caab924e541846cfb42399ff0104c2becba22210
Tobias Happ <tobias.happ@gmx.de>
2021-02-13 16:47:26 +0100

    Extract normalize page input

--- src/templates/index.js
+++ src/templates/index.js
@@ -4,12 +4,10 @@ import React from 'react'
 import Layout from '../components/base/Layout'
 import Slider from '../components/index/Slider'
 import InfoBoxes from '../components/index/InfoBoxes'
+import { normalizePageInput } from '../util/normalizer'
 
 const IndexPage = ({ data, entry }) => {
-  // data is not set in preview mode
-  let dataSet = data
-    ? data.markdownRemark.frontmatter
-    : entry.getIn(['data']).toJS()
+  let dataSet = normalizePageInput(data, entry)
 
   return (
     <Layout title={dataSet.title} description={dataSet.description}>
