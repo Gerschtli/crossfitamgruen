45c040402dc62ffac26e0348f371eef9ae6c97e3
Tobias Happ <tobias.happ@gmx.de>
2021-02-13 17:39:30 +0100

    Move styling of content to its component

--- src/components/templates/SimplePage.js
+++ src/components/templates/SimplePage.js
@@ -1,55 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
+import { graphql } from 'gatsby'
 
 import Layout from '../base/Layout'
 import Content from '../util/Content'
-import { breakpoints, mediaQuery } from '../../util/breakpoints'
-import { graphql } from 'gatsby'
 import { normalizePageInputWithBody } from '../../util/normalizer'
 
-const StyledContent = styled(Content)`
-  margin: 0 auto;
-  max-width: ${breakpoints.md}em;
-  padding: 1em;
-
-  ${mediaQuery(breakpoints.sm)} {
-    padding-left: 1.5em;
-    padding-right: 1.5em;
-  }
-
-  p {
-    margin-top: 1em;
-    margin-bottom: 1em;
-    line-height: 1.4;
-  }
-
-  ul,
-  ol {
-    padding-left: 2em;
-  }
-
-  ul p,
-  ol p,
-  ul ul,
-  ul ol,
-  ol ul,
-  ol ol {
-    margin-top: 0;
-    margin-bottom: 0;
-  }
-
-  // necessary for preview
-  img {
-    display: block;
-    margin: 0 auto;
-  }
-`
 const SimplePage = ({ data, entry, widgetFor }) => {
   const { dataSet, html } = normalizePageInputWithBody(data, entry, widgetFor)
 
   return (
     <Layout title={dataSet.title} description={dataSet.description}>
-      <StyledContent content={html} isPreview={!data} />
+      <Content content={html} isPreview={!data} />
     </Layout>
   )
 }
--- src/components/util/Content.js
+++ src/components/util/Content.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Content = ({ content, className, isPreview }) => {
+import { breakpoints, mediaQuery } from '../../util/breakpoints'
+
+const ContentComponent = ({ content, className, isPreview }) => {
   if (isPreview) {
     return <main className={className}>{content}</main>
   }
@@ -9,5 +12,43 @@ const Content = ({ content, className, isPreview }) => {
     <main className={className} dangerouslySetInnerHTML={{ __html: content }} />
   )
 }
+
+const Content = styled(ContentComponent)`
+  margin: 0 auto;
+  max-width: ${breakpoints.md}em;
+  padding: 1em;
+
+  ${mediaQuery(breakpoints.sm)} {
+    padding-left: 1.5em;
+    padding-right: 1.5em;
+  }
+
+  p {
+    margin-top: 1em;
+    margin-bottom: 1em;
+    line-height: 1.4;
+  }
+
+  ul,
+  ol {
+    padding-left: 2em;
+  }
+
+  ul p,
+  ol p,
+  ul ul,
+  ul ol,
+  ol ul,
+  ol ol {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  // necessary for preview
+  img {
+    display: block;
+    margin: 0 auto;
+  }
+`
 
 export default Content
