ae97cac9164dd4ee1ae44fc2a99d0fe36ceaae91
Tobias Happ <tobias.happ@gmx.de>
2021-02-14 12:43:06 +0100

    Add preview templates for contact and contactSuccess

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,14 @@
 import CMS from 'netlify-cms-app'
 
 import { default as wrapper } from '../util/withStyledComponentsRendered'
+import ContactPage from '../templates/contact'
+import ContactSuccessPage from '../templates/contactSuccess'
 import DisclosurePage from '../templates/disclosure'
 import ImprintPage from '../templates/imprint'
 import IndexPage from '../templates/index'
 
+CMS.registerPreviewTemplate('contact', wrapper(ContactPage))
+CMS.registerPreviewTemplate('contactSuccess', wrapper(ContactSuccessPage))
 CMS.registerPreviewTemplate('disclosure', wrapper(DisclosurePage))
 CMS.registerPreviewTemplate('imprint', wrapper(ImprintPage))
 CMS.registerPreviewTemplate('index', wrapper(IndexPage))
