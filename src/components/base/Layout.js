b0187653f9e09fafced99027c60a0882053b4fe0
Tobias Happ <tobias.happ@gmx.de>
2021-02-16 17:32:15 +0100

    Move all icons in one directory

--- src/components/base/Layout.js
+++ src/components/base/Layout.js
@@ -28,22 +28,26 @@ const Layout = ({ children, title, description, isPreview }) => {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-touch-icon.png"
+        href="/icons/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="/icons/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="/icons/favicon-16x16.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <link
+        rel="mask-icon"
+        href="/icons/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
       <meta name="apple-mobile-web-app-title" content="CrossFit am Gr&uuml;n" />
       <meta name="application-name" content="CrossFit am Gr&uuml;n" />
       <meta name="msapplication-TileColor" content="#da532c" />
